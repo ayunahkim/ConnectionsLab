848bf6b39f46ac84cab9bb5ad38c0e9e7b3bd6af
ayunahkim <79731952+ayunahkim@users.noreply.github.com>
2025-09-27 22:41:39 +0900

    fix barcode font, added comments

--- week3/script.js
+++ week3/script.js
@@ -2,45 +2,49 @@ let responded, submitted;
 let answer = null;
 let alignment;
 let test = 0;
-
-
 let positive,neutral,negative=[];
 let randind;
-    
+
+
+//fetch api 
 fetch("https://cors-anywhere.com/https://eightballapi.com/api/categories?locale=en")
     .then(response => response.json())
     .then(data => {
+        //save the fetched data into 3 different arrays based on category
         positive = data.positive;
         neutral = data.neutral;
         negative = data.negative;
-        //console.log("sending to respond function");
+
+        //call respond function
         respond();
     });
 
+//respond function
 function respond(){
-    //console.log("responding");
+    //generate random number from 0-2
     let randnum = Math.floor(Math.random()*3);
 
-    //positive
+    //positive - if random number is 0
     if(randnum==0){
         randind = Math.floor(Math.random()*9);
         answer = positive[randind];
         alignment = "positive";
     }
 
-    //neutral
+    //neutral - if random number is 1
     else if(randnum==1){
         randind = Math.floor(Math.random()*4);
         answer = neutral[randind];
         alignment = "neutral";
     }
 
-    //negative
+    //negative - if random number is 2
     else if(randnum==2){
         randind = Math.floor(Math.random()*4);
         answer = negative[randind];
         alignment = "negative";
     }
-    //console.log(answer);
+
+    //set variable that holds if this function ran as true
     responded = true;
 }
--- week3/sketch.js
+++ week3/sketch.js
@@ -1,24 +1,30 @@
+let p = -1; //page number, starts at -1 for title screen
+let c = 0; //counter for timing certain "dialogue" switches
 
-let p = -1;
-let c = 0;
-let chaogif,resultchao, resultlove,egg;
-let hatching,crystalball,questionchao;
-let chaotype = "neutral";
+//variables for holding images
+let chaogif,resultchao, resultlove, egg, hatching, crystalball, questionchao, chaoID, fruits;
+
+//variables for holding created chao data
+let chaotype = "neutral"; //default values are neutral
 let chaodata, chaoname, chaopersonality, chaofav;
-let chaoID, fruits;
+
+//variable for saving the chao id on the last pages
 let outputIMG;
+let font;
 
 
+//runs when user submits their answer
 function submitting(){
     submitted = true;
-    console.log(submitted);
-    p=1;
+    p=1; //set page to 1
     
+    //hide the element with the input box on page after submitting
     document.getElementById("centerbox").style.visibility = "hidden";
 }
 
-
+//preload assets - default values are set to neutral aligned chao
 function preload(){
+    //load images
     chaogif = loadImage("assets/chaofloat.gif");
     resultchao = loadImage("assets/neutral.png");
     resultlove = loadImage("assets/neutrallove.png");
@@ -27,17 +33,23 @@ function preload(){
     crystalball = loadImage("assets/fortuneball.png");
     questionchao = loadImage("assets/neutralquestion.png");
     titlescreen = loadImage("assets/titlescreen.png");
-    chaodata = loadJSON("chao.json");
     chaoID = loadImage("assets/neutralid.png");
+
+    //load chao data (not api, local json file)
+    chaodata = loadJSON("chao.json");
+
+    font = loadFont("assets/barcode.ttf");
 }
 
+//runs once when program starts
 function setup(){
+    //create variable holding the HTML div "canvasbox", create canvas then attach to the canvasbox div
     let canvasBox = document.getElementById("canvasbox");
-
     let canvas = createCanvas(800,500);
     canvas.parent(canvasBox);
 
-    frameRate(6);
+    frameRate(6); //set framerate lower to put less strain on everything
+
     imageMode(CENTER);
     noStroke();
     fill("#8b81a6");
@@ -47,61 +59,64 @@ function setup(){
     rectMode(CENTER);
 }
 
+//continuously runs
 function draw(){
     background("#d7f4ff");
-    //clear();
 
+    //only starts running the main part of the program if the api data has been fetched and saved into the answer variable
     if(answer){
         pages();
 
+        //once user submits their question, chao will be created and assets set to reflect changes
         if(submitted){
             Alignment();
         }
     } 
 }
 
+//the main program is held here
 function pages(){
-    if(p==-1){
+    if(p==-1){ //title screen
         image(titlescreen,width/2,height/2);
     }
-    if(p==0){
+    if(p==0){ //input screen
         image(egg,width/2,height/2);
         image(crystalball,width/2,height/2);
         document.getElementById("centerbox").style.visibility = "visible";
     }
-    else if(p==1){
-        frameRate(10);
+    else if(p==1){ //loading page, animation runs
+        frameRate(10); //frameRate set faster so it does not take as long
         loadingFortune();
     }
-    else if(p==2){
+    else if(p==2){ //click to hatch egg page
         image(egg,width/2,height/2);
         text("oh? your egg is hatching!",width/2,400);
         text("click to hatch!",width/2,420);
-        c = 0;
+        c = 0; //reset counter to 0 after using it in loadingFortune()
     }
-    else if(p==3){
+    else if(p==3){ //hatching animation page
         c++;
         image(hatching,width/2,height/2);
-        if(c>40){
+        if(c>40){ //once animation ends based on counter value, go to next page
             p++;
         }
     }
-    else if(p==4){
-        frameRate(6);
+    else if(p==4){ //reveal chao page
+        frameRate(6); //set frameRate back to a slower speed
         push();
-        textSize(40);
+        textSize(50);
         text("TADA!!!",width/2,height/2);
         pop();
         image(resultchao,width/4,height/2);
     }
-    else if(p==5){
+    else if(p==5){ //result chao and info box pop up page
         image(resultchao,width/4,height/2);
     }
-    else if(p==6){
+    else if(p==6){ //what was the answer to your wish?
         text("wonder what the answer to your wish was?",width/2,450);
         image(questionchao,width/2,height/2);
     }
-    else if(p==7){
+    else if(p==7){ //8ball api result page
         push();
         textSize(50);
         textStyle(BOLD);
@@ -109,13 +124,13 @@ function pages(){
         pop();
         image(resultchao,width/2,height/2);
     }
-    else if(p==8){
+    else if(p==8){ //chao loves you no matter what the result was!
         image(resultlove,width/2,height/2);
         textSize(20);
         text("no matter the result, your chao loves you!",width/2,450);
         textSize(14);
     }
-    else if(p==9){
+    else if(p==9){ //chao id display and save function shown
         frameRate(1);
         idInfo();
         text("use the save button below to save your chao's id card!",width/2,460);
@@ -123,81 +138,86 @@ function pages(){
     }
 }
 
+//runs on page 1 loading animation
 function loadingFortune(){
-    c++;
+    c++; //start counter
 
     image(chaogif,width/2,height/2);
+
     push();
     fill(255);
+    //changing dialogue based on counter value
     if(c<20){
-        text("reading your fortune",width/2,height/2);
+        text("reading your fortune...",width/2,height/2);
     } else if(c>20&&c<40){
-        text("communicating with the stars",width/2,height/2);
+        text("communicating with the stars...",width/2,height/2);
     } else if(c>40&&c<60){
-        text("it's becoming clear",width/2,height/2);
+        text("it's becoming clear...",width/2,height/2);
     }
     pop();
     
+    //once counter hits 60, go to page 2
     if(c>60){
         p=2;
     }
-    
-
 }
 
+//setting images and values to reflect the chao alignment
 function Alignment(){
-    if(alignment=="positive"){
+    if(alignment=="positive"){ //if positive, set assets and values to hero chao
         chaotype = "hero";
         resultchao = loadImage("assets/hero.png");
         resultlove = loadImage("assets/herolove.png");
         questionchao = loadImage("assets/heroquestion.png");
         chaoID = loadImage("assets/heroid.png");
     }
-    else if(alignment=="neutral"){
+    else if(alignment=="neutral"){ //if neutral, keep chaotype neutral and default assets
         chaotype = "neutral";
     }
-    else if(alignment=="negative"){
+    else if(alignment=="negative"){ //if negative, set assets and values to dark chao
         chaotype = "dark";
         resultchao = loadImage("assets/dark.png");
         resultlove = loadImage("assets/darklove.png");
         questionchao = loadImage("assets/darkquestion.png");
         chaoID = loadImage("assets/darkid.png");
     }
-
 }
 
+//runs whenever mouse is clicked
 function mouseClicked(){
+    //if mouse is within canvas bounds
     if(mouseX>0&&mouseX<800&&mouseY>0&&mouseY<500){
+        //if page is not input screen or loading screen, go to next page on mouseclick
         if(p!=0 && p!=1){
-            if(p==2){
+            if(p==2){ //initiate create chao before going to next page
                 createChao();
             }
-            if(p==4){
+            if(p==4){ //initiate info box before going to next page
                 infoBox();
             }
-            if(p==5){
+            if(p==5){ //make infobox hidden before going to next page
                 document.getElementById("popup").style.visibility = "hidden";
             }
-            if(p==8){
+            if(p==8){ //make save button visible before going to next page
                 document.getElementById("savebttn").style.visibility = "visible";
             }
-            if(p==9){
+            if(p==9){ //make save button hidden and reload actual tab page instead of going to next
                 document.getElementById("savebttn").style.visibility = "hidden";
                 window.location.reload();
             }
             p++;
-            
         }
-        
-
     }
 }
 
+//take data from chaodata file and set the variables
 function createChao(){
+    //pick randomly from the arrays in the json file
     chaoname = random(chaodata.name);
     chaopersonality = random(chaodata.personality);
     chaofav = random(chaodata.favorite);
 
+    //load relevant image based on chaofav result
     if(chaofav=="Round Fruit"){
         fruits = loadImage("assets/circlefruits.png");
     } else if(chaofav == "Triangle Fruit"){
@@ -205,10 +225,9 @@ function createChao(){
     } else if(chaofav == "Square Fruit"){
         fruits = loadImage("assets/squarefruits.png");
     }
-
-    //console.log("chao created");
 }
 
+//display information in HTML box displayed over canvas
 function infoBox(){
     let popup = document.getElementById("popup");
     popup.innerHTML = `<h3>Chao Information</h3>
@@ -216,9 +235,9 @@ function infoBox(){
                     <p><b>Personality: </b>${chaopersonality}</p>
                     <p><b>Favorite fruit: </b>${chaofav}</p>`;
     popup.style.visibility = "visible";
-    console.log("infobox ran");
 }
 
+//text displayed on the chao ID cards
 function idInfo(){
     image(chaoID,width/2,height/2);
     image(fruits,width/2,height/2);
@@ -248,13 +267,13 @@ function idInfo(){
 
     push();
     textAlign(CENTER);
-    textFont("Libre Barcode 39",40);
-    text("placeholder",490,360);
+    textSize(40);
+    textFont(font);
+    text(chaoname,490,360);
     pop();
 }
 
 function saveChao(){
-    //console.log(mouseX,mouseY);
     outputIMG = get(142,94,521,309);
-    save(outputIMG);
+    save(outputIMG,"mychao.png");
 }
